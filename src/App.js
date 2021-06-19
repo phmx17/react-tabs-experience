e96f1b35ffc38b67b5e2904d69125d43015dfc34
Slim Yelow <slimyelow@gmail.com>
2021-06-19 12:27:30 +0200

    add fetchJobs() to load data from api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,66 @@
 import React, { useState, useEffect } from 'react'
 import { FaAngleDoubleRight } from 'react-icons/fa'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
 const url = 'https://course-api.com/react-tabs-project'
+
 function App() {
-  return <h2>tabs project setup</h2>
+  const [loading, setLoading] = useState(false)
+  const [data, setData] = useState([])
+  const [value, setValue] = useState(0)
+
+  const fetchJobs = async() => {
+    setLoading(true)
+    try {
+      let response = await fetch(url)
+      let newJobs = await response.json()
+      setData(newJobs)
+      setLoading(false)
+      
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+  } 
+
+  // on page load
+  useEffect(()=> {
+    fetchJobs()
+  }, [])
+
+  // let obj = data[0]
+  // console.log(obj[0])
+
+  if (loading) {
+    return <section className="section loading">
+      <h1>loading...</h1>
+    </section>
+  }
+  return <main className="jobs-center">
+    <section className="section">
+      <div className="btn-container">
+        {
+
+        }
+   
+        <button className="job-btn">Anya</button>
+        <button className="job-btn">Taylor</button>
+        <button className="job-btn">Joy</button>
+      </div>
+      <div className="job-info">
+        <h3>Full stack</h3>
+        <h4>Anya</h4>
+          <p className="job-date">December 1</p>
+        <div className="job-desc">        
+          <FaAngleDoubleRight className="job-icon"/>         
+          <div>
+            <p>Description</p>
+            <p>Description</p>
+          </div>
+        </div>
+
+      </div>
+    </section>
+  </main>
 }
 
 export default App
