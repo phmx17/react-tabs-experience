ca4072b32bebf2523c9d5b743404e2afb745d549
Slim Yelow <slimyelow@gmail.com>
2021-06-19 15:29:04 +0200

    add buttons the reference each item in the jobs array

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 const url = 'https://course-api.com/react-tabs-project'
 
 function App() {
-  const [loading, setLoading] = useState(false)
-  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [jobs, setJobs] = useState([])
   const [value, setValue] = useState(0)
 
   const fetchJobs = async() => {
@@ -13,9 +13,8 @@ function App() {
     try {
       let response = await fetch(url)
       let newJobs = await response.json()
-      setData(newJobs)
+      setJobs(newJobs)
       setLoading(false)
-      
     } catch (error) {
       setLoading(false)
       console.log(error)
@@ -25,16 +24,49 @@ function App() {
   // on page load
   useEffect(()=> {
     fetchJobs()
-  }, [])
-
-  // let obj = data[0]
-  // console.log(obj[0])
+  }, [])  
 
   if (loading) {
     return <section className="section loading">
       <h1>loading...</h1>
     </section>
   }
+  
+  const { company, dates, duties, title } = jobs[value]
+  return <section className="section">
+    <div className="title">
+      <h2>experience</h2>
+      <div className="underline"></div>
+    </div>
+    <div className="jobs-center">
+      {/* btn container */}
+      <div className="btn-container">
+        {jobs.map((job, index)=> {
+          return <button className="job-btn" key={job.id} onClick={()=> setValue(index)}>{job.company}</button>
+        })}
+      </div>
+      {/* job info */}
+      <article className="job-info">
+        <h3>{title}</h3>
+        <h4>{company}</h4>
+        <p className="job-date">{dates}</p>
+        {duties.map((duty, index) => {
+          return <div className="job-desc" key={index}>
+            <FaAngleDoubleRight className="job-icon"/>
+            <p>{duty}</p>
+          </div>
+        })}
+      </article>
+    </div>
+  </section>
+
+
+
+
+
+
+
+
   return <main className="jobs-center">
     <section className="section">
       <div className="btn-container">
